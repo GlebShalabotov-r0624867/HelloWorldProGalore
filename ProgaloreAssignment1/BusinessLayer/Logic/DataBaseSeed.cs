66d444ca434b649d7139fc4d8198832cbc49aa2d
Gleb S <gleb0406@hotmail.com>
2020-03-27 12:57:32 +0100

    first assignment

---
+++ ProgaloreAssignment1/BusinessLayer/Logic/DataBaseSeed.cs
@@ -0,0 +1,39 @@
+﻿using BusinessLayer.Models;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Logic
+{
+    public class DataBaseSeed
+    {
+        public List<User> DB { get; set; }
+
+        public DataBaseSeed()
+        {
+            DB = new List<User>
+            {
+                new User
+                {
+                    Name = "Gleb",
+                    LastName = "Shalabotov",
+                    Age = 23,
+                    Mail = "gleb@be.be",
+                    Password = "T"
+                },
+
+                new User
+                {
+                    Name = "Marlies",
+                    LastName = "Goderis",
+                    Age = 21,
+                    Mail = "marlies@be.be",
+                    Password = "t"
+                }
+            };
+        }
+    }
+}
---
+++ ProgaloreAssignment1/ProgaloreAssignment1/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using BusinessLayer.Logic;
+using BusinessLayer.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ProgaloreAssignment1.Models;
+
+namespace ProgaloreAssignment1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+         
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult Overview()
+        {
+            var db = new DataBaseSeed();
+
+            var model = new UserDatabase
+            {
+                Users = db.DB
+            };
+
+            return View(model);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
